68254d2b2870a9e5d446cc6a5ca71182ebb7ddfd
rummanakter <rummanakter660@gmail.com>
2023-03-23 16:42:34 +0600

    assignment-1

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+
+function multiplicationTable(num) {
+  let i;  
+  for (i = 1; i <= 10; i++) {
+    let product = num * i;
+    console.log(num+"*"+i+"="+product);
+  }
+}
+
+multiplicationTable(7);
+
+
